ff1f082615d43b6e2fce1d195959f3f13a4fd772
yuexin <610696148@qq.com>
2020-04-09 16:23:58 +0800

    Fix: 初始化

--- src/main/java/com/company/project/service/FileService.java
+++ src/main/java/com/company/project/service/FileService.java
@@ -4,11 +4,6 @@ import com.company.project.model.File;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 public interface FileService {
     File uploadFileToMongo(MultipartFile file);
 
